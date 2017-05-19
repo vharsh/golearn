f04d7f9f61d9bd8f6e1e5f0813ce05032329a62e
Harsh Vardhan <harsh59v@gmail.com>
2017-05-19 15:38:32 +0530

    Switched to os Module for taking command-line arguments

--- fetchSource.go
+++ fetchSource.go
@@ -1,26 +1,23 @@
-package main      //package should compile as an executable program and not as a shared library
+package main    //package should compile as an executable program and not as a shared library
 
 import (
   "fmt"
-  "flag"        //command line arguments with the predefined methods 
   "os"          //system calls
   "net/http"    //retrieving the source-code of the webpage requested
   "io/ioutil"   //reads and prints the fetched source-code
-
 )
 
 func main() {
-    flag.Parse()
-    args := flag.Args()
-    fmt.Println(args)
+	args := os.Args[1:]
     if len(args) < 1 {
-    fmt.Println("Please Enter the URL")
-    os.Exit(1)
-  }
-    retrieve(args)
+	fmt.Fprintln(os.Stderr, "Please Enter the URL")
+	os.Exit(1)
+    }
+    fmt.Println("Downloading: ", args)
+    retrieve(args[0])
 }
 
-func retrieve(url string){// Prints the source code
+func retrieve(url string) {// Prints the source code
     resp, err := http.Get(url)
     if err != nil{
       fmt.Println("read error is:", err)
