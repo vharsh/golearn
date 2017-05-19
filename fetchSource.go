11068147af9f0e9ff37dd7c4c0f81b39aaa48c22
Harsh Vardhan <harsh59v@gmail.com>
2017-05-19 11:16:07 +0530

    Trims extra whitespaces and comments

--- fetchSource.go
+++ fetchSource.go
@@ -1,42 +1,37 @@
-package main      //The package “main” tells the Go compiler that the package should compile as an executable program 
-                  //instead of a shared library.
+package main      //package should compile as an executable program and not as a shared library
 
 import (
-  "fmt"         //importing the package that contains some predefined methods.
-// "flag"        //importing this package to do something with the command line arguments with the predefined methods 
-                //existing in this package. 
-  "os"          //importing this package for the system calls. 
-  "net/http"    //importing this package for retrieving the source-code of the webpage requested.
-  "io/ioutil"   //importing this package for reading and printing the source-code fetched.
+  "fmt"
+  "flag"        //command line arguments with the predefined methods 
+  "os"          //system calls
+  "net/http"    //retrieving the source-code of the webpage requested
+  "io/ioutil"   //reads and prints the fetched source-code
 
 )
 
 func main() {
-                 
-    flag.Parse()      
+    flag.Parse()
     args := flag.Args()
-    fmt.Println(args)       
-    if len(args) < 1 {        
-    fmt.Println("Please Enter the URL")  
-    os.Exit(1)                                
+    fmt.Println(args)
+    if len(args) < 1 {
+    fmt.Println("Please Enter the URL")
+    os.Exit(1)
   }
-    retrieve(args)        //call the retrieve function                                   
+    retrieve(args)
 }
 
-func retrieve(url string){          //gives the  source code as output.
-    
-    resp, err := http.Get(url)         
+func retrieve(url string){// Prints the source code
+    resp, err := http.Get(url)
     if err != nil{
       fmt.Println("read error is:", err)
-      return 
+      return
     }
     body, err := ioutil.ReadAll(resp.Body);
-    if err != nil{ 
+    if err != nil{
       fmt.Println("read error is:", err)
-      return 
+      return
     } else{
       fmt.Println(string(body))
-      
     }
 }
 
